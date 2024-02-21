23e9aace6e87028c604723c14f85abfea04d78a0
raja-dettex <rk6091039@gmail.com>
2024-02-21 18:35:54 +0300

    refactored to proxy reqeust that is hitting the domain

--- devoxx/src/main.rs
+++ devoxx/src/main.rs
@@ -1,7 +1,9 @@
-use std::{error::Error, net::SocketAddr};
+use std::{borrow::Borrow, error::Error, net::SocketAddr};
 
-use axum::{extract::Host, http::{HeaderMap, Request, Response, StatusCode}, response::IntoResponse, routing::*, Router};
+use axum::{extract::Host, http::{method, uri::{self, PathAndQuery}, HeaderMap, Request, Response, StatusCode}, response::IntoResponse, routing::*, Router};
 use miette::IntoDiagnostic;
+use reqwest::Method;
+
 
 const PROXY_ORGIN_URI : &'static str = "localhost:3000";
 const PROXY_FROM_DOMAIN : &'static str = "client.hello";
@@ -14,17 +16,26 @@ async fn main() -> Result<(), &'static str> {
     Ok(())
 }
 
-async fn proxy_handler<Body>( host : Host, request : Request<Body>) -> Result<impl IntoResponse, String> {
+async fn proxy_handler<Body>( host : Host, req_headers: HeaderMap, method : axum::http::method::Method,  request : Request<Body>,) -> Result<impl IntoResponse, String> {
     let uri = request.uri();
-    let split : Vec<_>= host.0.split(':').collect();
+    let split : Vec<_>= host.0.split(':').collect(); 
     let host_name = split[0];
     println!("host :{}", host_name);
     if host_name != PROXY_FROM_DOMAIN {
         return Err(format!("expected host {} but found {:#?}", PROXY_FROM_DOMAIN.to_string(), host));
     }
-    let path = uri.path_and_query().map(|pq| pq.path()).unwrap_or("/");
+    //let path = uri.path_and_query().cloned().map(|pq| pq.path()).unwrap_or("/");
+    let p_and_q = uri.path_and_query().cloned().unwrap_or_else(|| PathAndQuery::from_static("/"));
+    let url  = uri::Builder::new().scheme("http")
+        .authority(PROXY_ORGIN_URI)
+        .path_and_query(p_and_q.clone())
+        .build()
+        .map_err(|_| "could not build url")?;
     let client = reqwest::Client::new();
-    let response = client.get(format!("http://{}{}", PROXY_ORGIN_URI, path))
+    println!("method :{} and url :{}", method, url);
+    let response = client
+        .request(method, url.to_string())
+        .headers(req_headers)
         .send()
         .await
         .map_err(|_| "request failed").unwrap();
